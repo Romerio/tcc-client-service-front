3556201e3055bf1345488c0b3d1d0549234814b8
Romério Sousa <romerio.unix@gmail.com>
2018-04-05 03:43:04 -0300

    Host para producao e dev

--- src/modules/main/config.js
+++ src/modules/main/config.js
@@ -2,6 +2,6 @@ const API_DOMAIN = process.env.API_HOST ? process.env.API_HOST : "http://localho
 const SOCKET_PATH = process.env.API_HOST ? `${process.env.API_HOST}/auth` : "http://localhost:3000/auth";
 
 module.exports = {
-	API_DOMAIN,
-	SOCKET_PATH
+	API_DOMAIN:"https://front-client-a.herokuapp.com",
+	SOCKET_PATH :"https://front-client-a.herokuapp.com/auth"
 };
