85dec2a1c8b4215e0f6e10c60e52c467635b8baa
Romério Sousa <romerio.unix@gmail.com>
2018-04-05 03:14:49 -0300

    Host para producao e devb

--- src/modules/main/config.js
+++ src/modules/main/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	API_DOMAIN: process.env.NODE_ENV === "production" ? "https://back-client-a.herokuapp.com" : "http://localhost:3000",
-	SOCKET_PATH: process.env.NODE_ENV === "production" ?"https://back-client-a.herokuapp.com/auth" : "http://localhost:3000/auth"
+	API_DOMAIN: "https://back-client-a.herokuapp.com",//process.env.NODE_ENV === "production" ? "https://back-client-a.herokuapp.com" : "http://localhost:3000",
+	SOCKET_PATH: "https://back-client-a.herokuapp.com/auth" // process.env.NODE_ENV === "production" ?"https://back-client-a.herokuapp.com/auth" : "http://localhost:3000/auth"
 };
