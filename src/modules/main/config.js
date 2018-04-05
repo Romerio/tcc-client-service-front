8ee104ea273c8135191640643fe78e9aa1edd652
Romério Sousa <romerio.unix@gmail.com>
2018-04-05 03:31:13 -0300

    Host para producao e dev

--- src/modules/main/config.js
+++ src/modules/main/config.js
@@ -1,4 +1,7 @@
+const API_DOMAIN = process.env.API_HOST ? process.env.API_HOST : "http://localhost:3000";
+const SOCKET_PATH = process.env.API_HOST ? `${process.env.API_HOST}/auth` : "http://localhost:3000/auth";
+
 module.exports = {
-	API_DOMAIN: process.env.API_HOST ? process.env.API_HOST : "http://localhost:3000",
-	SOCKET_PATH: process.env.API_HOST ? process.env.API_HOST + "/auth" : "http://localhost:3000/auth"
+	API_DOMAIN,
+	SOCKET_PATH
 };
