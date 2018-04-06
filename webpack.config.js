9258e3fe78c9d0b8fe8236d5ab93b531e948a401
Romério Sousa <romerio.unix@gmail.com>
2018-04-06 11:02:35 -0300

    Ajuste em variaveis de ambiente

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,7 @@ module.exports = {
 		new extractTextPlugin("app.css"),
 		new webpack.DefinePlugin({
 			"process.env": JSON.stringify({ 
-				NODE_ENV: process.env.NODE_ENV || "development",
-				API_HOST: process.env.API_HOST ||  "http://localhost:3000",
-				TEST: process.env.TEST
+				...process.env
 			})
 		}),
 	],
